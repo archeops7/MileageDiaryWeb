7137883a88522e5efd3cbc2920d28449f6ba1440
archeops7 <snowcorvette@gmail.com>
2020-08-11 14:57:07 +0900

    replace manage.py

--- MileageDiaryWeb2/src/MileageDiaryWeb2/settings.py
+++ MileageDiaryWeb2/src/MileageDiaryWeb2/settings.py
@@ -19,7 +19,6 @@ load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -137,6 +136,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL='DiaryApp:login'     # ログイン
 LOGOUT_URL='DiaryApp:logout'
